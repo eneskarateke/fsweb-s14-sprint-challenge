291de1249924ced227605dd0b39ff46c4f9673a3
eneskarateke <eneskarateke@gmail.com>
2023-06-23 15:37:28 +0300

    projects api yapıldı, hatalar var

--- api/project/model.js
+++ api/project/model.js
@@ -1,2 +1,23 @@
 // `Proje` modeli buraya
-const db = require("../../data/db-config");
+const db = require("../../data/dbConfig");
+
+async function projectGetir() {
+  let projects = await db("projects as pj").select("pj.*");
+
+  return projects;
+}
+
+async function findById(project_id) {
+  const project = await db("projects").where("project_id", project_id).first();
+  return project;
+}
+
+async function projectEkle(project) {
+  const [project_id] = await db("projects").insert(project);
+  return findById(project_id);
+}
+
+module.exports = {
+  projectGetir,
+  projectEkle,
+};
--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,25 @@
 //  `/api/projects` router buraya
+const router = require("express").Router();
+
+const ProjectModel = require("./model");
+
+router.get("/", async (req, res, next) => {
+  try {
+    const projects = await ProjectModel.projectGetir();
+    res.json(projects);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  const project = req.body;
+  try {
+    const insertedProject = await ProjectModel.projectEkle(project);
+    res.status(201).json(insertedProject);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
