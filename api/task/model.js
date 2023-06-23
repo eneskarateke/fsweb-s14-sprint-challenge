fad3361cccee46beae88302e020c96489cf0a6d9
eneskarateke <eneskarateke@gmail.com>
2023-06-23 15:44:10 +0300

    tasks tamamlandı, hatalar var

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,24 @@
 // bu`Task` modeli buraya
+const db = require("../../data/dbConfig");
+
+async function taskGetir() {
+  let tasks = await db("tasks as ts").select("ts.*");
+
+  return tasks;
+}
+
+async function findById(task_id) {
+  const task = await db("tasks").where("task_id", task_id).first();
+  return task;
+}
+
+async function taskEkle(task) {
+  const [task_id] = await db("tasks").insert(task);
+  return findById(task_id);
+}
+
+module.exports = {
+  taskGetir,
+
+  taskEkle,
+};
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,26 @@
 // `/api/tasks` router buraya
+
+const router = require("express").Router();
+
+const TaskModel = require("./model");
+
+router.get("/", async (req, res, next) => {
+  try {
+    const tasks = await TaskModel.taskGetir();
+    res.json(tasks);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  const task = req.body;
+  try {
+    const insertedtask = await TaskModel.taskEkle(task);
+    res.status(201).json(insertedtask);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
